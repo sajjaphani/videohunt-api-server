baae0343852f5cd245e8cc5c6850e9d1d2d71dc4
Phani Sajja <sajja.phani@gmail.com>
2017-07-16 21:20:16 +0530

    Updated to emit the id rather than _id, removed __v

--- models/index.js
+++ models/index.js
@@ -4,10 +4,10 @@ var CommentSchema = require('./Comment')
 var FeedSchema = require('./Feed')
 
 var models = function (db) {
-    var User = db.model('User', UserSchema);
-    var Post = db.model('Post', PostSchema);
-    var Comment = db.model('Comment', CommentSchema);
-    var Feed = db.model('Feed', FeedSchema);
+    var User = db.model('User', transform(UserSchema));
+    var Post = db.model('Post', transform(PostSchema));
+    var Comment = db.model('Comment', transform(CommentSchema));
+    var Feed = db.model('Feed', transform(FeedSchema));
 
     return {
         User: User,
@@ -15,6 +15,29 @@ var models = function (db) {
         Comment: Comment,
         Feed: Feed
     };
+}
+
+function transform(Schema) {
+    Schema.set('toObject', { virtuals: true })
+    Schema.set('toJSON', { virtuals: true })
+
+    if (!Schema.options.toJSON)
+        Schema.options.toJSON = {};
+
+    Schema.options.toJSON.transform = function (doc, ret, options) {
+        delete ret._id
+        delete ret.__v
+    }
+
+    Schema.options.toJSON.virtuals = true;
+
+    Schema.options.toObject.transform = function (doc, ret, options) {
+        delete ret._id
+    }
+
+    Schema.options.toObject.virtuals = true;
+
+    return Schema
 }
 
 module.exports = models;
